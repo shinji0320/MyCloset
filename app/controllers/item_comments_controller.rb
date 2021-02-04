db49946fafec3a5aeae0636fed5f3d3abf56aeb0
shinji0320 <shinji.blue.one@gmail.com>
2021-02-04 08:50:54 +0000

    comment_create_fix

--- app/controllers/item_comments_controller.rb
+++ app/controllers/item_comments_controller.rb
@@ -1,21 +1,24 @@
 class ItemCommentsController < ApplicationController
   def create
-    item = Item.find(params[:Item_id])
-    comment = current_user.item_comments.new(item_comment_params)
-    comment.item_id = item.id
-    comment.save
-    redirect_to item_path(item)
+    @item = Item.find(params[:item_id])
+    @item_comment = ItemComment.new(item_comment_params)
+    @item_comment.item_id = @item.id
+    @item_comment.user_id = current_user.id
+    @item_comment.save
+    redirect_to items_path
   end
-  
+
   def destroy
-    ItemComment.find_by(id: params[:id], item_id: params[:item_id]).destroy
-    redirect_to item_path(params[:item_id])
+    @item = Item.find(params[:Item_id])
+    @item_comment.item_id = @item.id
+    @item_comment.destroy
+    redirect_to items_path
   end
-  
+
   private
 
   def item_comment_params
-    params.require(:item_comment).permit(:comment, :score)
+    params.require(:item_comment).permit(:comment)
   end
-    
+
 end
