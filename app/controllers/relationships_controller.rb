0b37889a6b21392fa19febae3a5328fed52121e6
shinji0320 <shinji.blue.one@gmail.com>
2021-02-07 04:04:34 +0000

    user/show_fix

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,26 +1,29 @@
 class RelationshipsController < ApplicationController
    before_action :set_user
-
+  
   def create
     following = current_user.follow(@user)
-    if following.save
-      flash[:success] = 'ユーザーをフォローしました'
-      redirect_to @user
-    else
-      flash.now[:alert] = 'ユーザーのフォローに失敗しました'
-      redirect_to @user
-    end
+    following.save
+    flash[:success] = 'ユーザーをフォローしました'
+    redirect_to request.referer
   end
 
   def destroy
     following = current_user.unfollow(@user)
-    if following.destroy
-      flash[:success] = 'ユーザーのフォローを解除しました'
-      redirect_to @user
-    else
-      flash.now[:alert] = 'ユーザーのフォロー解除に失敗しました'
-      redirect_to @user
-    end
+    following.destroy
+    flash[:success] = 'ユーザーのフォローを解除しました'
+    redirect_to request.referer
+  end
+
+  
+  def followings
+    user = User.find(params[:user_id])
+    @users = user.followings
+  end
+
+  def followers
+    user = User.find(params[:user_id])
+    @users = user.followers
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,8 @@ Rails.application.routes.draw do
   end
   resources :users, only: [:index, :show, :edit, :update]
   resources :relationships, only: [:create, :destroy]
+  get 'followings' => 'relationships#followings', as: 'followings'
+  get 'followers' => 'relationships#followers', as: 'followers'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
