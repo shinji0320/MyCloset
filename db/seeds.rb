25d4e0e5a0b5984f84ee4736ea867c52d61ca2e8
shinji0320 <shinji.blue.one@gmail.com>
2021-02-12 07:57:05 +0000

    item_demo/user_demo_add

--- db/seeds.rb
+++ db/seeds.rb
@@ -27,3 +27,63 @@ Genre.create!(
         }
     ]
 )
+User.create!(
+    [
+        {
+            name: "test",
+            email: "1@1",
+            password: "111111",
+            introduction: "テスト用に作成したデータです"
+        },
+        {
+            name: "ミニーちゃん",
+            email: "mini@example.com",
+            password: "111111",
+            introduction: "休みの日はウィンドウショピングしてます"
+        },
+        {
+            name: "デイジー",
+            email: "deig@example.com",
+            password: "111111",
+            introduction: "趣味は映画でホラー大好きです"
+        },
+        {
+            name: "キャサリン",
+            email: "casarin@example.com",
+            password: "111111",
+            introduction: "持っている服はワンピースが多めなのでワンピースのおすすめ紹介してます"
+        },
+        {
+            name: "ピーチ姫",
+            email: "peach@example.com",
+            password: "111111",
+            introduction: "ドレスしか持っていないわ"
+        },
+        {
+            name: "キノピコ",
+            email: "kinopiko@example.com",
+            password: "111111",
+            introduction: "フォフォフォフォフォ"
+        }
+    ]
+)
+Item.create!(
+    [
+        {
+            genre_id: "1",
+            user_id: "1",
+            name: "赤のスニーカー",
+            shop_name:"GU",
+            detail: "1990円でコスパ最高です",
+            private: "ture"
+        },
+        {
+            genre_id: "2",
+            user_id: "1",
+            name: "ピンクのワンピース",
+            shop_name:"GU",
+            detail: "1290円でセール中で安かったー",
+            private: "false"
+        }
+    ]
+)
