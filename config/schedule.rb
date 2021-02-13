29be6fa10f22c427d4fd3fd8ed6dbe17c6eda08a
shinji0320 <shinji.blue.one@gmail.com>
2021-02-13 03:04:39 +0000

    fix

--- config/schedule.rb
+++ config/schedule.rb
@@ -23,11 +23,17 @@ require File.expand_path(File.dirname(__FILE__) + "/environment")
 rails_env = Rails.env.to_sym
 set :environment, rails_env
 set :output, 'log/cron.log'
-every 2.minute do
-  begin
-    runner "Batch::DataReset.data_reset"
-  rescue => e
-    Rails.logger.error("aborted rails runner")
-    raise e
-  end
-end
+
+# every 2.minute do
+#   rails 'db:reset'
+#   rails 'db:seed'
+# end
+
+# every 2.minute do
+#   begin
+#     runner "Batch::DataReset.data_reset"
+#   rescue => e
+#     Rails.logger.error("aborted rails runner")
+#     raise e
+#   end
+# end
--- lib/batch/data_reset.rb
+++ lib/batch/data_reset.rb
@@ -1,7 +1,8 @@
 class Batch::DataReset
   def self.data_reset
-    User.delete_all
-    Item.delete_all
-    p "ユーザー・商品を全て削除しました"
+    # rails 'db:reset'
+    # User.delete_all
+    # Item.delete_all
+    # p "ユーザー・商品を全て削除しました"
   end
 end
