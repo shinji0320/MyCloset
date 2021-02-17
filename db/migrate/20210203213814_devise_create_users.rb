f0e92b3db125551cca3eb0212e256e8fc353eec5
shinji0320 <shinji.blue.one@gmail.com>
2021-02-17 00:57:21 +0000

    user_prefecture_add

--- db/migrate/20210203213814_devise_create_users.rb
+++ db/migrate/20210203213814_devise_create_users.rb
@@ -6,6 +6,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.2]
       ## Database authenticatable
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
+      t.string :prefecture
 
       ## Recoverable
       t.string   :reset_password_token
@@ -31,7 +32,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.2]
       # t.integer  :failed_attempts, default: 0, null: false # Only if lock strategy is :failed_attempts
       # t.string   :unlock_token # Only if unlock strategy is :email or :both
       # t.datetime :locked_at
-      
+
       t.string :name
       t.text :introduction
       t.string :profile_image_id
--- db/schema.rb
+++ db/schema.rb
@@ -80,6 +80,7 @@ ActiveRecord::Schema.define(version: 2021_02_14_024038) do
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
+    t.string "prefecture"
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
