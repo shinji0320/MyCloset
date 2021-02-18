fc4ed34ceccdbaa88d220d4b20af6d9e0a52db2d
shinji0320 <shinji.blue.one@gmail.com>
2021-02-18 08:10:32 +0000

    top_view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -23,7 +23,7 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:name, :profile_image, :introduction)
+    params.require(:user).permit(:name, :profile_image, :introduction, :prefecture)
   end
 
   def ensure_user
