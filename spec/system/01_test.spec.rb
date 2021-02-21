8e3e176d7f52ae63a8754f631369fb722c2ca304
shinji0320 <shinji.blue.one@gmail.com>
2021-02-21 05:28:30 +0000

    rsec_system_test1_fix

--- spec/system/01_test.spec.rb
+++ spec/system/01_test.spec.rb
@@ -10,21 +10,17 @@ describe '[STEP1] ユーザログイン前のテスト' do
       it 'URLが正しい' do
         expect(current_path).to eq '/'
       end
-      it 'Sign Upリンクの内容が正しい' do
-        sign_up_link = find_all('a')[6].native.inner_text
-        expect(page).to have_link sign_up_link, href: new_user_registration_path
-      end
     end
   end
 
   describe 'アバウト画面のテスト' do
     before do
-      visit '/home/about'
+      visit '/homes/about'
     end
 
     context '表示内容の確認' do
       it 'URLが正しい' do
-        expect(current_path).to eq '/home/about'
+        expect(current_path).to eq '/homes/about'
       end
     end
   end
@@ -36,36 +32,29 @@ describe '[STEP1] ユーザログイン前のテスト' do
 
     context '表示内容の確認' do
       it 'タイトルが表示される' do
-        expect(page).to have_content 'Bookers'
+        expect(page).to have_content 'MyCloset'
       end
     end
 
     context 'リンクの内容を確認' do
       subject { current_path }
 
-      it 'Homeを押すと、トップ画面に遷移する' do
-        home_link = find_all('a')[1].native.inner_text
-        home_link = home_link.delete(' ')
-        home_link.gsub!(/\n/, '')
-        click_link home_link
-        is_expected.to eq '/'
-      end
-      it 'Aboutを押すと、アバウト画面に遷移する' do
+      it 'aboutを押すと、アバウト画面に遷移する' do
         about_link = find_all('a')[2].native.inner_text
         about_link = about_link.gsub(/\n/, '').gsub(/\A\s*/, '').gsub(/\s*\Z/, '')
         click_link about_link
-        is_expected.to eq '/home/about'
+        is_expected.to eq '/homes/about'
       end
-      it 'sign upを押すと、新規登録画面に遷移する' do
-        signup_link = find_all('a')[3].native.inner_text
-        signup_link = signup_link.gsub(/\n/, '').gsub(/\A\s*/, '').gsub(/\s*\Z/, '')
-        click_link signup_link
+      it 'sign_upを押すと、新規登録画面に遷移する' do
+        sign_up_link = find_all('a')[3].native.inner_text
+        sign_up_link = sign_up_link.gsub(/\n/, '').gsub(/\A\s*/, '').gsub(/\s*\Z/, '')
+        click_link sign_up_link
         is_expected.to eq '/users/sign_up'
       end
-      it 'loginを押すと、ログイン画面に遷移する' do
-        login_link = find_all('a')[4].native.inner_text
-        login_link = login_link.gsub(/\n/, '').gsub(/\A\s*/, '').gsub(/\s*\Z/, '')
-        click_link login_link
+      it 'sign_inを押すと、ログイン画面に遷移する' do
+        sign_in_link = find_all('a')[4].native.inner_text
+        sign_in_link = sign_in_link.gsub(/\n/, '').gsub(/\A\s*/, '').gsub(/\s*\Z/, '')
+        click_link log_in_link
         is_expected.to eq '/users/sign_in'
       end
     end
