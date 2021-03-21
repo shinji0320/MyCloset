8dc4ca36e315d5a072483eda2301f2c105f8a6d1
shinji0320 <shinji.blue.one@gmail.com>
2021-03-21 06:24:22 +0000

    S3本番環境へ反映

--- config/initializers/refile.rb
+++ config/initializers/refile.rb
@@ -1,6 +1,6 @@
 require 'refile/s3'
 
-if Rails.env.production? # 本番環境の場合はS3へアップロード
+#if Rails.env.production? # 本番環境の場合はS3へアップロード
   aws = {
     access_key_id: ENV['S3_ACCESS_KEY_ID'], # アクセスキーID
     secret_access_key: ENV['S3_SECRET_ACCESS_KEY'], # シークレットアクセスキー
